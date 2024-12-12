2553453ac5a44beff9c5b8fcf20c5543eea85293
MatteoSB11 jgjg <matteosbarra07@gmail.com>
2024-12-12 09:30:49 +0000

    Punto 4

--- Sbarra/src/app/app.component.ts
+++ Sbarra/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from './container/container.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Sbarra/src/app/macchina/macchina.component.ts
+++ Sbarra/src/app/macchina/macchina.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-macchina',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './macchina.component.css'
 })
 export class MacchinaComponent {
-
+  @Input() nomemacchina!: string ;
 }
